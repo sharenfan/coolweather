7b8a2d2ca6b8d62cad78eccf0ca343e3605bdcdf
shanrenfan <2540019996@qq.com>
2017-06-15 21:59:41 +0800

    完成编历省市县三级列表的功能

---
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -0,0 +1,8 @@
+package com.coolweather.android;
+
+/**
+ * Created by Death on 2017/6/15.
+ */
+
+public class ChooseAreaFragment {
+}
---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,8 @@
+package com.coolweather.android.util;
+
+/**
+ * Created by Death on 2017/6/15.
+ */
+
+public class HttpUtil {
+}
